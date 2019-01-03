3ca37d97cfc5ffb6ce42271d0da2ebd6b4ab675c
Hoai Nguyen <hoaius7@gmail.com>
2019-01-02 21:45:15 -0600

    Passing Route Parameters

--- react-tut-ajax/src/containers/Blog/Blog.js
+++ react-tut-ajax/src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import './Blog.css';
 import Posts from "./Posts/Posts";
 import {Route, NavLink} from "react-router-dom";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
     render() {
@@ -31,6 +32,7 @@ class Blog extends Component {
                 {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" component={FullPost} />
             </div>
         );
     }
