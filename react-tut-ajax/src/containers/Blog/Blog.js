695493c59ecebb83f56ad3ac9dc648005637ce9a
Hoai Nguyen <hoaius7@gmail.com>
2019-01-04 13:17:10 -0600

    Understanding Nested Routes

--- react-tut-ajax/src/containers/Blog/Blog.js
+++ react-tut-ajax/src/containers/Blog/Blog.js
@@ -3,7 +3,6 @@ import './Blog.css';
 import Posts from "./Posts/Posts";
 import {Route, NavLink, Switch} from "react-router-dom";
 import NewPost from "./NewPost/NewPost";
-import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
     render() {
@@ -13,13 +12,12 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                                to="/"
-                                exact
+                                to="/posts/"
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
-                                }}>Home</NavLink></li>
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -31,9 +29,8 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
                 {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
                 <Switch>
-                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
         );
