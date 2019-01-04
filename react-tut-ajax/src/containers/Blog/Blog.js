fb1e78fa3f35e88cc39f6b661898cd32bb1cde42
Hoai Nguyen <hoaius7@gmail.com>
2019-01-04 14:10:11 -0600

    Working with Guards

--- react-tut-ajax/src/containers/Blog/Blog.js
+++ react-tut-ajax/src/containers/Blog/Blog.js
@@ -5,6 +5,10 @@ import {Route, NavLink, Switch, Redirect} from "react-router-dom";
 import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
+    state = {
+        auth: false
+    };
+
     render() {
         return (
             <div className="Blog">
@@ -29,7 +33,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
                 {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
