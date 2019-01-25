5b06e1b8c2680bb5d7e6f412c63cd8dfc6e2e405
Hoai Nguyen <hoaius7@gmail.com>
2019-01-25 16:13:57 -0600

    Handling Asynchronous Code

--- react-redux-tut/src/store/actions/actions.js
+++ react-redux-tut/src/store/actions/actions.js
@@ -31,16 +31,24 @@ export const subtract = (value) => {
     };
 };
 
-export const storeResult = (res) => {
+export const saveResult = (res) => {
     return {
         type: STORE_RESULT,
         result: res
     };
-};
+}
+
+export const storeResult = (res) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(saveResult(res));
+        }, 2000);
+    }
+}
 
 export const deleteResult = (resElId) => {
     return {
         type: DELETE_RESULT,
         resultElId: resElId
     };
-};
+}
