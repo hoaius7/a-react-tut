437b3b06939fa827232f71fe116496c9675247ca
Hoai Nguyen <hoaius7@gmail.com>
2019-01-25 13:03:14 -0600

    Adding  Middleware

--- react-redux-tut/src/index.js
+++ react-redux-tut/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './App';
@@ -14,9 +14,21 @@ const rootReducer = combineReducers({
     counterReducer: counterReducer,
     resultReducer: resultReducer
 })
-const store = createStore(rootReducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
